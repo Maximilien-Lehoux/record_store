f6df6fe21c3f193d7d7c4bb2a7c18437d2d3ccf2
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2020-10-02 18:01:24 +0200

    django fonctionne

---
+++ help.py
@@ -0,0 +1,24 @@
+# virtual env :
+# virtualenv -p python3 env
+
+# activer environnement virtuel :
+# env\Scripts\activate.bat (cmd)
+
+# Désactiver environnement virtuel :
+# desactivate (cmd)
+
+# version django :
+# python -m django --version (cmd)
+
+# Créér projet django
+# django-admin startproject nom_projet_django (cmd)
+
+# Base de donnée django (postgresql)
+# pip install psycopg2
+
+# Installer posgresql windows via le site
+
+# Activation django :
+# python manage.py runserver
+# $ python manage.py makemigrations
+# $ python manage.py migrate
