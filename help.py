4cbebfa80fdd113ead70c2a32d018cddb56883a9
Maximilien Lehoux <maximilien.lehoux@gmail.com>
2020-10-03 12:51:01 +0200

    création de l'app store

--- help.py
+++ help.py
@@ -22,3 +22,25 @@
 # python manage.py runserver
 # $ python manage.py makemigrations
 # $ python manage.py migrate
+
+# modifier dasns settings : la langue (fr) et TIME_ZONE = 'Europe/Paris'
+
+# django debug toolbar qui permet de manipuler une barre qui donne plusieurs fonctionnalité pendant le developpement
+# pip install django-debug-toolbar
+# dans settings.py dans installed_apps, rajouter 'debug_toolbar',
+# rajouter le middleware : 'debug_toolbar.middleware.DebugToolbarMiddleware',
+# INTERNAL_IPS = ['127.0.0.1'] à la fin après static_url
+
+# dans urls.py
+# from django.contrib import admin
+# from django.urls import path
+# from django.conf import settings
+# from django.conf.urls import include, url
+# if settings.DEBUG:
+    # import debug_toolbar
+    # urlpatterns = [
+        # url(r'^__debug__/', include(debug_toolbar.urls)),
+    # ] + urlpatterns
+
+# Création d'une nouvelle application django :
+# django-admin startapp store (cmd)
